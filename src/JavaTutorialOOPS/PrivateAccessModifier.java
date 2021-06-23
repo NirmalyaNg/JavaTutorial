89a74f6e2b64c3dd0d1488725bb48b2d9eec51bb
Nirmalya Ganguly <nirmalyaganguly1999@gmail.com>
2021-06-23 19:04:07 +0530

    OOps

---
+++ src/JavaTutorialOOPS/PrivateAccessModifier.java
@@ -0,0 +1,42 @@
+package JavaTutorialOOPS;
+
+class MyEmployee {
+    private String name;
+    private int salary;
+
+    public String getName(){
+        return this.name;
+    }
+    public int getSalary(){
+        return this.salary;
+    }
+
+    public void setName(String s){
+        this.name = s;
+    }
+
+    public void setSalary(int sal){
+        this.salary = sal;
+    }
+}
+
+
+public class PrivateAccessModifier {
+    public static void main(String[] args) {
+        MyEmployee emp1 = new MyEmployee();
+        MyEmployee emp2 = new MyEmployee();
+
+        emp1.setName("Nirmalya");
+        emp1.setSalary(28000);
+
+        emp2.setName("Sampriti");
+        emp2.setSalary(28000);
+
+        // emp1.name = "Tubai";  --> Will give error since name has private access modifier
+        System.out.println(emp1.getName());
+        System.out.println(emp1.getSalary());
+
+        System.out.println(emp2.getName());
+        System.out.println(emp2.getSalary());
+    }
+}
